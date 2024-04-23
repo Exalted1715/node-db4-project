f2a47416b23a022d02d7eef1a3fa9bba4366bd87
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-23 15:20:52 -0600

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,12 @@
-function getRecipebyId(recipe_id){
-    return Promise.resolve(`Sweet recipe with id, ${recipe_id}`)
+const db = require('../../data/db-config')
+
+
+
+
+async function getRecipebyId(recipe_id){
+    const recipeRows = await db('recipes as r')
+    return recipeRows
+    .where('recipe_id', recipe_id)
 }
 
 module.exports = {getRecipebyId}
